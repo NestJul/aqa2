9750c8c8ab8cb5c620c0d24d8367eddae2c70b40
NestJul <nestjul@yandex.ru>
2025-02-15 15:31:35 +0700

    aqa2 post body data

--- src/test/java/ru/netology/rest/RestTestV1.java
+++ src/test/java/ru/netology/rest/RestTestV1.java
@@ -20,7 +20,7 @@ class RestTestV1 {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
